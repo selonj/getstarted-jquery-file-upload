b27542271e19390eb57113122db58b903c3ef696
feexon <feexon@foxmail.com>
2016-07-16 18:20:59 +0800

    upload files to server side

---
+++ src/main/java/com/feexon/FileUploadServlet.java
@@ -0,0 +1,166 @@
+package com.feexon;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+
+import static org.apache.commons.fileupload.disk.DiskFileItemFactory.DEFAULT_SIZE_THRESHOLD;
+import static org.apache.commons.io.FileUtils.copyFile;
+import static org.apache.commons.io.FileUtils.copyInputStreamToFile;
+import static org.apache.commons.io.FilenameUtils.getExtension;
+import static org.apache.commons.io.FilenameUtils.getName;
+import static org.apache.commons.io.IOUtils.closeQuietly;
+
+/**
+ * Created by Administrator on 2016-07-16.
+ */
+public class FileUploadServlet extends HttpServlet {
+  private static final String PARAM_TEMP_DIR = "tempDir";
+  private static final String PARAM_UPLOAD_DIR = "uploadDir";
+  private static final String DEFAULT_TEMP_DIR = "temp";
+  private static final String DEFAULT_UPLOAD_DIR = "upload";
+  private String tempDir = DEFAULT_TEMP_DIR;
+  private String uploadDir = DEFAULT_UPLOAD_DIR;
+  private ServletFileUpload multipartParser;
+
+  @Override public void init() throws ServletException {
+    super.init();
+    setParameters();
+    createMultipartParser();
+  }
+
+  private void setParameters() {
+    tempDir = getInitParameter(PARAM_TEMP_DIR, DEFAULT_TEMP_DIR);
+    uploadDir = getInitParameter(PARAM_UPLOAD_DIR, DEFAULT_UPLOAD_DIR);
+  }
+
+  private String getInitParameter(String name, String defaultValue) {
+    if (isBlank(getInitParameter(name))) return defaultValue;
+    return getInitParameter(name);
+  }
+
+  private void createMultipartParser() throws ServletException {
+    try {
+      multipartParser = new ServletFileUpload(createTempFileStoreIfNecessary());
+    } catch (IOException e) {
+      throw new ServletException(e);
+    }
+  }
+
+  private FileItemFactory createTempFileStoreIfNecessary() throws IOException {
+    return new DiskFileItemFactory(DEFAULT_SIZE_THRESHOLD, mkdir(in(tempDir)));
+  }
+
+  private File mkdir(File directory) throws IOException {
+    FileUtils.forceMkdir(directory);
+    return directory;
+  }
+
+  @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    renderTo(req, resp, file(getName(req.getRequestURI()), in(uploadDir)));
+  }
+
+  private void renderTo(HttpServletRequest req, HttpServletResponse resp, File file) throws IOException {
+    ServletOutputStream out = resp.getOutputStream();
+    try {
+      copyFile(file, out);
+    } finally {
+      closeQuietly(out);
+    }
+  }
+
+  @Override protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    if (isMultipart(req)) {
+      try {
+        List<String> filenames = saveUploadFiles(req);
+        respondTo(req, resp, filenames);
+      } catch (IOException e) {
+        throw e;
+      } catch (Exception e) {
+        throw new ServletException(e);
+      }
+    }
+  }
+
+  private boolean isMultipart(HttpServletRequest req) {
+    return ServletFileUpload.isMultipartContent(req);
+  }
+
+  private List<String> saveUploadFiles(HttpServletRequest req) throws FileUploadException, IOException {
+    List<String> filenames = new ArrayList<>();
+    for (FileItem item : multipartParser.parseRequest(req)) filenames.add(save(item));
+    return filenames;
+  }
+
+  private void respondTo(HttpServletRequest req, HttpServletResponse resp, List<String> filenames) throws IOException {
+    PrintWriter out = resp.getWriter();
+    out.write(paths(req, filenames).toString());
+    out.close();
+  }
+
+  private JSONArray paths(HttpServletRequest req, List<String> filenames) {
+    JSONArray paths = new JSONArray();
+    for (String filename : filenames) paths.put(path(req, filename));
+    return paths;
+  }
+
+  private String save(FileItem item) throws IOException {
+    InputStream source = item.getInputStream();
+    try {
+      String filename = generateFileName(item.getName());
+      copyInputStreamToFile(source, file(filename, in(uploadDir)));
+      return filename;
+    } finally {
+      closeQuietly(source);
+    }
+  }
+
+  private File in(String path) {
+    return path.startsWith("/") ? new File(path.substring(1)) : file(path);
+  }
+
+  private File file(String path) {
+    return new File(getServletContext().getRealPath(path));
+  }
+
+  private File file(String filename, File parent) {
+    return new File(parent, filename);
+  }
+
+  private String path(HttpServletRequest req, String file) {
+    return req.getServletPath() + "/" + file;
+  }
+
+  private String generateFileName(String filename) {
+    if (hasNoExtension(filename)) return randomName();
+    return randomName() + "." + getExtension(filename);
+  }
+
+  private String randomName() {
+    return UUID.randomUUID().toString();
+  }
+
+  private boolean hasNoExtension(String filename) {
+    return isBlank(getExtension(filename));
+  }
+
+  private boolean isBlank(String source) {
+    return source == null || source.isEmpty();
+  }
+}
---
+++ src/test/java/com/feexon/FileUploadTest.java
@@ -0,0 +1,114 @@
+package com.feexon;
+
+import java.io.IOException;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONTokener;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by Administrator on 2016-07-16.
+ */
+public class FileUploadTest {
+  private final String contextPath = "/jquery-file-upload";
+  private final int serverPort = 1000;
+  private final HttpClient client = HttpClients.createDefault();
+  private final Server server = new Server(serverPort);
+
+  @Before public void setUp() throws Exception {
+    server.setStopAtShutdown(true);
+    server.setHandler(runAsWebApplication());
+    server.start();
+  }
+
+  private Handler runAsWebApplication() {
+    WebAppContext webapp = new WebAppContext();
+    webapp.setWar("src/main/webapp");
+    webapp.setContextPath(contextPath);
+    webapp.addOverrideDescriptor("/WEB-INF/web.xml");
+    return webapp;
+  }
+
+  @After public void tearDown() throws Exception {
+    server.stop();
+  }
+
+  @Test public void uploadsFileToServer() throws Exception {
+    String content = "test";
+
+    String location = theLocationOf(uploaded(file("readme.md", content)));
+
+    assertThat(location, endsWith(".md"));
+
+    assertThat(theContentOf(fetched(location)), equalTo(content));
+  }
+
+  private HttpResponse uploaded(HttpEntity file) throws IOException {
+    return execute(postWithEntity(toURL("/upload"), file));
+  }
+
+  private HttpResponse fetched(String file) throws IOException {
+    return execute(new HttpGet(toURL(file)));
+  }
+
+  private String theContentOf(HttpResponse response) throws IOException {
+    return IOUtils.toString(response.getEntity().getContent());
+  }
+
+  private String theLocationOf(HttpResponse response) throws IOException, JSONException {
+    return toJSONArray(theContentOf(response)).getString(0);
+  }
+
+  private JSONArray toJSONArray(String content) throws IOException, JSONException {
+    return new JSONArray(new JSONTokener(content));
+  }
+
+  private HttpResponse execute(HttpUriRequest request) throws IOException {
+    HttpResponse response = client.execute(request);
+    assertResponseThatOk(response);
+    return response;
+  }
+
+  private HttpUriRequest postWithEntity(String url, HttpEntity entity) {
+    HttpPost request = new HttpPost(url);
+    request.setEntity(entity);
+    return request;
+  }
+
+  private HttpEntity file(String filename, String content) {
+    return MultipartEntityBuilder.create().addBinaryBody("files", content.getBytes(), ContentType.APPLICATION_OCTET_STREAM, filename).build();
+  }
+
+  private String toURL(String path) {
+    if (!path.startsWith("/")) return path;
+    return baseURL() + path;
+  }
+
+  private String baseURL() {
+    return String.format("http://localhost:%d%s", serverPort, contextPath);
+  }
+
+  private void assertResponseThatOk(HttpResponse response) {
+    assertThat(response.getStatusLine().getStatusCode(), equalTo(HttpStatus.SC_OK));
+  }
+}
