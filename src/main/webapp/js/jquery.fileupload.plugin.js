bbfbfdf0696ce1c49df3cf172a1f3977d23ae544
feexon <feexon@foxmail.com>
2016-07-19 14:46:44 +0800

    jquery-file-upload upload multi-files ui demo

--- src/main/webapp/js/jquery.fileupload.plugin.js
+++ src/main/webapp/js/jquery.fileupload.plugin.js
@@ -1,13 +1,17 @@
 (function () {
-  var _progressing = function (visible, e, data) {
-    if ($.type(visible) != 'boolean') {
-      data = e;
-      e = visible;
+  $.extend($, {
+    toURL: function (url) {
+      if (!/^\//.test(url)) return url;
+      if (config && config.baseURL) {
+        return config.baseURL + url;
+      }
+      return url;
     }
-    var progress = parseInt(data.loaded / data.total * 100, 10);
-    $(this).find(data.selectors.progress).css('visibility', visible === false ? 'hidden' : 'visible').end().
-    find(data.selectors.progressing).width(progress + '%');
-  };
+  });
+  $.ajaxPrefilter(function (options) {
+    options.url = $.toURL(options.url);
+  });
+
   $.extend($.fn, {
     when: function (condition, callback) {
       var self = this;
@@ -26,39 +30,81 @@
     }
   });
 
-  var compSelector = '[data-toggle="fileupload"]';
-  $(document).on('click', compSelector, function (e) {
+  var selectors = {
+    singleton: '[data-toggle="fileupload"]',//
+    multiple: '[data-toggle="multi-upload"]',//
+    preview: '.fileupload-preview',//
+    progress: '.fileupload-progress',//
+    progressing: '.fileupload-progressing',//
+    remove: '[data-toggle="remove"]'
+  };
+
+  var _progressing = function (visible, e, data) {
+    if ($.type(visible) != 'boolean') {
+      data = e;
+      e = visible;
+    }
+    var progress = parseInt(data.loaded / data.total * 100, 10);
+    $(this).find(selectors.progress).css('visibility', visible === false ? 'hidden' : 'visible').end().
+    find(selectors.progressing).width(progress + '%');
+  };
+
+  var fileupload = window.fileupload = function (el) {
     var originalAdd = $.blueimp.fileupload.prototype.options.add;
-    $($(e.target).closest(compSelector)).fileupload({
+    return $(el).fileupload({
       //      fieldName:'',
-      selectors: {
-        preview: '.fileupload-preview',//
-        progress: '.fileupload-progress',//
-        progressing: '.fileupload-progressing'
-      },//
+      //
       dataType: 'json',//
       add: function (e, data) {
         var type = 'upload';
-        $(this).dequeue(type).queue(type, function (next) {
+        var self = $(this);
+        self.dequeue(type).queue(type, function (next) {
           data.abort();
           next();
+        });
+        self.find(selectors.remove).one('click', function () {
+          if (data.state() === 'pending') {
+            self.detach();
+            data.abort();
+          }
         });
         originalAdd.call(this, e, data);
       },//
       send: $.proxy(_progressing, true),//
       progress: $.proxy(_progressing),//
       done: function (e, data) {
-        var path = data.result.files[0];
-        var fieldName = data.fieldName;
-        $(this).find(data.selectors.preview).findIf('img', function () {
-          return $('<img>')
-        }).attr('src', path.substring(1)).end().when(fieldName, function () {
-          this.findIf('input[name="' + fieldName + '"]', function () {
-            return $('<input type="hidden">').attr('name', fieldName);
-          }).val(path)
+        var path = data.result.files[0], dfd = $.Deferred(), self = $(this);
+        var options = self.fileupload('option');
+        if (options.serviceUrl) {
+          var form = {};
+          form[options.serviceParamName] = path;
+          $.post(options.serviceUrl, form).done(dfd.resolve).fail(dfd.reject);
+        } else {
+          dfd.resolve();
+        }
+        dfd.done(function () {
+          var fieldName = options.fieldName;
+          self.find(selectors.preview).findIf('img', function () {
+            return $('<img>');
+          }).attr('src', $.toURL(path)).end().when(fieldName, function () {
+            this.findIf('input[name="' + fieldName + '"]', function () {
+              return $('<input type="hidden">').attr('name', fieldName);
+            }).val(path)
+          });
         });
       },//
       always: $.proxy(_progressing, null, false)
     });
+  };
+  $(document).on('click', selectors.singleton, function (e) {
+    fileupload($(e.target).closest(selectors.singleton));
   });
+  $(document).on('change', selectors.multiple, function (e) {
+    var file = $(e.target);
+    var control = file.closest(selectors.multiple);
+    var context = $($(control.attr('data-template')).html()).find('.fileupload-preview').width(control.width()).height(control.height()).end().insertAfter(control);
+    window.fileupload(context).fileupload('add', {fileInput: file});
+    file.val(null);
+  });
+
 })();
